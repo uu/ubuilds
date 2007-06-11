ab0a779a88e4935098eeae71a1055ca55732709d
kiorky <kiorky@fe387f86-7aec-0310-8f44-a148a97195aa>
2007-06-11 21:59:06 +0000

    Maven 2.1 Bump
    git-svn-id: https://overlays.gentoo.org/svn/proj/java/java-experimental@5064 fe387f86-7aec-0310-8f44-a148a97195aa

---
+++ dev-java/doxia/files/assemble-plexus-resources.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# use to facilitate resources tarballing after generating them
+# enjoy :p
+
+assemble() {
+	local dest=titi
+	mkdir dest
+	for i in $(find . -name generated-resources|sed -re "s:(/target)|(\./)::g");
+	do
+		local maven_dest="$dest/$(dirname $i)/src/main"
+		echo assemble "$i"
+		mkdir -p  "$maven_dest"
+		cp -rf \
+		"$(echo $i|sed -re "s:generated-resources:target/generated-resources:g")/plexus" \
+		"$maven_dest/resources"
+	done
+}
+
+assemble
+
