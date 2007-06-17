37d008e61592fb992ca6f0776a97326522657050
kiorky <kiorky@fe387f86-7aec-0310-8f44-a148a97195aa>
2007-06-17 18:07:25 +0000

    resurrecting alpha8 for maven shared
    git-svn-id: https://overlays.gentoo.org/svn/proj/java/java-experimental@5146 fe387f86-7aec-0310-8f44-a148a97195aa

--- dev-java/doxia/files/assemble-plexus-resources.sh
+++ dev-java/doxia/files/assemble-plexus-resources.sh
@@ -14,6 +14,15 @@ assemble() {
 		"$(echo $i|sed -re "s:generated-resources:target/generated-resources:g")/plexus" \
 		"$maven_dest/resources"
 	done
+ 	for i in $(find . -name generated-sources|sed -re "s:(/target)|(\./)::g");
+	do
+		local maven_dest="$dest/$(dirname $i)/src/main"
+		echo assemble "$i"
+		mkdir -p  "$maven_dest"
+		cp -rf \
+		"$(echo $i|sed -re "s:generated-sources:target/generated-sources:g")/modello" \
+		"$maven_dest/java"
+	done 
 }
 
 assemble
